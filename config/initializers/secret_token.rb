f29a3028824f1b4f808df91186542c5f2ad02e85
westcott <daniel.s.westcott@gmail.com>
2013-10-05 17:36:42 -0700

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wpcrm4::Application.config.secret_token = '552640c635276a29f3889a34054a1a7eec0a0cdfb6aec45436e4faee0c016010b930b54ef8119d1f65ff828e0bb5c1c56cc91fb607bb260e9abbf1dd0ae9b6f3'
